af8d17603943c02fa753d714c059ee3a392453a1
faster2020 <sergey.ganus@mail.ru>
2020-05-16 16:27:35 +0300

    Added new functions

--- js/script.js
+++ js/script.js
@@ -5,44 +5,187 @@ document.addEventListener('DOMContentLoaded', function () {
     modalBlock = document.querySelector('#modalBlock'),
     closeModal = document.querySelector('#closeModal'),
     questionTitle = document.querySelector('#question'),
-    formAnswers = document.querySelector('#formAnswers');
+    formAnswers = document.querySelector('#formAnswers'),
+    buttonBurger = document.getElementById('buttonBurger'),
+    buttonPrev = document.getElementById('prev'),
+    buttonNext = document.getElementById('next');
+
+  // const simple = 'простой';
+  // const string = `Я люблю "${simple}" JavaScript`;
+  // console.log(string);
+
+  // for (let i = 0; i <= 5; i++) {
+  //   console.log(i);
+  // }
+
+  const resizeWindow = () => {
+    const clientWidth = document.documentElement.clientWidth;
+
+    if (clientWidth < 751) {
+      buttonBurger.style.display = 'flex';
+    } else {
+      buttonBurger.style.display = 'none';
+    }
+  };
 
   const playTest = () => {
-    const question = {
-      title: 'Какого цвета бургер выберите?',
-      answers: [
-        {
-          id: 'answerItem1',
-          title: 'Стандарт',
-          image: './img/burger.png',
-        },
-        {
-          id: 'answerItem2',
-          title: 'Черный',
-          image: './img/burgerBlack.png',
-        },
-      ],
-    };
+    let numberQuestion = 0;
+    const questions = [
+      {
+        question: 'Какого цвета бургер?',
+        answers: [
+          {
+            id: '00',
+            title: 'Стандарт',
+            url: './img/burger.png',
+          },
+          {
+            id: '01',
+            title: 'Черный',
+            url: './img/burgerBlack.png',
+          },
+        ],
+        type: 'radio',
+      },
+      {
+        question: 'Из какого мяса котлета?',
+        answers: [
+          {
+            id: '10',
+            title: 'Курица',
+            url: './img/chickenMeat.png',
+          },
+          {
+            id: '11',
+            title: 'Говядина',
+            url: './img/beefMeat.png',
+          },
+          {
+            id: '12',
+            title: 'Свинина',
+            url: './img/porkMeat.png',
+          },
+        ],
+        type: 'radio',
+      },
+      {
+        question: 'Дополнительные ингредиенты?',
+        answers: [
+          {
+            id: '20',
+            title: 'Помидор',
+            url: './img/tomato.png',
+          },
+          {
+            id: '21',
+            title: 'Огурец',
+            url: './img/cucumber.png',
+          },
+          {
+            id: '22',
+            title: 'Салат',
+            url: './img/salad.png',
+          },
+          {
+            id: '23',
+            title: 'Лук',
+            url: './img/onion.png',
+          },
+        ],
+        type: 'checkbox',
+      },
+      {
+        question: 'Добавить соус?',
+        answers: [
+          {
+            id: '30',
+            title: 'Чесночный',
+            url: './img/sauce1.png',
+          },
+          {
+            id: '31',
+            title: 'Томатный',
+            url: './img/sauce2.png',
+          },
+          {
+            id: '32',
+            title: 'Горчичный',
+            url: './img/sauce3.png',
+          },
+        ],
+        type: 'radio',
+      },
+    ];
 
-    const renderQuestion = question => {
-      questionTitle.textContent = question.title;
-      formAnswers.textContent = '';
+    // questions[0].answers.forEach((item, index, arr) => {
+    //   console.log(item);
+    //   console.log(index);
+    //   console.log(arr);
+    // });
 
-      question.answers.forEach(({ id, title, image }) => {
-        const answer = `
-          <div class="answers-item d-flex flex-column">
-            <input type="radio" id="${id}" name="answer" class="d-none" />
-            <label for="${id}" class="d-flex flex-column justify-content-between">
-              <img class="answerImg" src="${image}" alt="burger" />
-              <span>${title}</span>
-            </label>
-          </div>
-        `;
-        formAnswers.insertAdjacentHTML('beforeend', answer);
+    const renderAnswers = indexQuestion => {
+      const type = questions[indexQuestion].type;
+      questions[indexQuestion].answers.forEach(({ id, title, url }) => {
+        const answerItem = document.createElement('div');
+        answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+        answerItem.insertAdjacentHTML(
+          'afterbegin',
+          `
+          <input type="${type}" id="${id}" name="answer" class="d-none" />
+          <label for="${id}" class="d-flex flex-column justify-content-between">
+            <img class="answerImg" src="${url}" alt="burger" />
+            <span>${title}</span>
+          </label>
+          `
+        );
+
+        formAnswers.appendChild(answerItem);
       });
     };
 
-    renderQuestion(question);
+    const renderButtons = indexQuestion => {
+      if (indexQuestion < 1) {
+        buttonPrev.style.display = 'none';
+      } else {
+        buttonPrev.style.display = '';
+      }
+
+      if (indexQuestion > questions.length - 2) {
+        buttonNext.style.display = 'none';
+      } else {
+        buttonNext.style.display = '';
+      }
+    };
+
+    const renderQuestions = indexQuestion => {
+      questionTitle.textContent = questions[indexQuestion].question;
+      formAnswers.textContent = '';
+
+      renderAnswers(indexQuestion);
+      renderButtons(indexQuestion);
+    };
+
+    const prevQuestion = () => {
+      numberQuestion--;
+
+      renderQuestions(numberQuestion);
+      renderButtons(numberQuestion);
+    };
+
+    const nextQuestion = () => {
+      numberQuestion++;
+
+      renderQuestions(numberQuestion);
+      renderButtons(numberQuestion);
+    };
+
+    renderQuestions(numberQuestion);
+
+    // buttonPrev.addEventListener('click', prevQuestion);
+    // buttonNext.addEventListener('click', nextQuestion);
+
+    buttonPrev.onclick = prevQuestion;
+    buttonNext.onclick = nextQuestion;
   };
 
   btnOpenModal.addEventListener('click', () => {
@@ -52,11 +195,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
   closeModal.addEventListener('click', () => {
     modalBlock.classList.remove('d-block');
+    buttonBurger.classList.remove('active');
   });
 
   modalBlock.addEventListener('click', event => {
     if (event.target.className === modalBlock.className) {
       modalBlock.classList.remove('d-block');
+      buttonBurger.classList.remove('active');
     }
   });
+
+  buttonBurger.addEventListener('click', () => {
+    buttonBurger.classList.add('active');
+    modalBlock.classList.add('d-block');
+    playTest();
+  });
+
+  window.addEventListener('resize', resizeWindow);
+
+  resizeWindow();
 });
